74389a5a18b5b1072027d57d5bebeac0b50df55b
Mushaheed Syed <syed.mushaheed@binarynumbers.io>
2021-03-11 17:07:39 +0530

    Add ndcajkvbkav

--- WebApplication1/BeanBag1.aspx.cs
+++ WebApplication1/BeanBag1.aspx.cs
@@ -10,17 +10,79 @@ using System.Web.UI.WebControls;
 namespace WebApplication1
 {
     public partial class BeanBag1 : System.Web.UI.Page
-    {
+    {   
+        protected bool isNotEmpty(String value) {
+            return value != null && value != "";
+        }
+
+        String BbName = ""; 
+        String BbType = ""; 
+        String BbRex = ""; 
+        String BbPrice = "";
+        String BbBeans = "";
+        String BbSize = "";
+
+        protected String constructQuery() {
+            var query = "SELECT * FROM BEANBAG WHERE "
+            if (isNotEmpty(Bbname)) {
+                query = query + "BbName = '" + BbName + "'";
+            }
+            if (isNotEmpty(BbType)) {
+                query = query + "BbType = '" + BbType + "'";
+            }
+            if (isNotEmpty(BbRex)) {
+                query = query + "BbRex = '" + BbRex + "'";
+            }
+            if (isNotEmpty(BbPrice)) {
+                query = query + "BbPrice = '" + BbPrice + "'";
+            }
+            if (isNotEmpty(BbBeans)) {
+                query = query + "BbBeans = '" + BbBeans + "'";
+            }
+            if (isNotEmpty(BbSize)) {
+                query = query + "BbSize = '" + BbSize + "'";
+            }
+            return query;
+        }
+
+        protected void updateValues() {
+            BbBeans = Beans.SelectedItem.Value.ToString();
+            BbSize = SIZE.SelectedValue;
+        }
+
+        protected void createRequest() {
+            updateValues();
+            query = constructQuery();
+            SqlCommand command = new SqlCommand(query, con);
+            SqlDataReader reader = command.ExecuteReader();
+            try
+            {
+                reader.Read();
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+            Bbname.Text = reader["BbName"].ToString();
+            Bbprice.Text = reader["BbPrice"].ToString();
+            BbType.Text = reader["BbType"].ToString();
+            BbRex.Text = reader["BbRex"].ToString();
+        }
+
+        SqlConnection con;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString);
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString);
             con.Open();
-            string str = "select * from BeanBag where  BbSize = '" + SIZE.SelectedValue + "'";
+
+            /// Get default values
+            BbSize = SIZE.SelectedValue;
+            string str = "select * from BeanBag where  BbSize = '" + BbSize + "'";
             SqlCommand com = new SqlCommand(str, con);
             SqlDataReader reader = com.ExecuteReader();
             reader.Read();
             Bbname.Text = reader["BbName"].ToString();
-            // reader.Read();
             Bbprice.Text = reader["BbPrice"].ToString();
         }
 
